2dfdefcec97104d19f92b85bdfbae5cf6a680338
Ahkal <bp@sojourn.llc>
2022-06-22 17:24:39 -0400

    Tests  stopProp. Uses e.preventDefault Utilize options to remove eventListener and more

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
 const button = document.querySelector('button');
+const scrollable = document.querySelector('#scrollable');
+// const abort = new AbortController();
+// button.addEventListener('click', onClick, {
+//     capture: true,
+//     once: true,
+//     passive: true,
+//     signal: abort.signal
+// });
+// abort.abort(); //removes event listener
 
-button.addEventListener('click', onClick);
-function onClick(e){
-    console.log('Button clicked!');
-    console.log(e);
-    console.log(e.type);
-    console.log(e.target);
-}
+// (event, function, useCapture)
+// function onClick(e){
+//     // e.preventDefault();
+//     // e.stopPropagation();
+//     console.log(e.target);
+//     console.log(this);
+// }
+
+// EVENTS
+// dblclick
+// mousedown
+// mouseup
+// keydown
+// keyup
+
+// Scrolling
+// mousemove
+// mouseenter
+
+// scrollable.addEventListener('scroll', e => {
+//     console.log(e.target.scrollTop);
+// })
+
+// scrollable.addEventListener('mouseenter', e => {
+//     console.log(e.pageX, e.pageY);
+// })
+
+// scrollable.addEventListener('mousemove', e => {
+//     console.log(e.pageX, e.pageY);
+// })
+
+// button.addEventListener('dragstart', e =>{
+//     console.log(e);
+// })
+
+// scrollable.addEventListener('drop', e =>{
+//     scrollable.prepend(button)
+// })
+
+// scrollable.addEventListener('dragover',e=>{
+//     // prevent default behavior because it prevents us from using drop.
+//     e.preventDefault();
+
+//     console.log('SAFE ZONE, PREP FOR LANDING');
+// })
